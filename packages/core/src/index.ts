65a5094419fa9e4e55fc70dcb00585908bf03307
Symphony <3516027002att@gmail.com>
2026-07-03 14:40:45 +0800

    code: export reference graph logic

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,5 @@ export * from './workspace/resourceUri.js';
 export * from './workspace/scanWorkspace.js';
 export * from './preview/openResourcePreview.js';
 export * from './patch/patchEngine.js';
+export * from './references/referenceBuilder.js';
+export * from './references/eventEvidence.js';
